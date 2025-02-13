0a510f46780f65810878274c7016d2027ba8e8e8
JotaLou <135313811+JotaLou@users.noreply.github.com>
2025-02-13 17:00:50 +0100

    añadir esqueleto autoclick

---
+++ ejercicios/autoclick_esqueleto.py
@@ -0,0 +1,7 @@
+''' Ejercicio del cursillo pragmatic python impartido en MediaLab
+    Enunciado: Hacer un script que pulse un botón en pantalla cada segundo.
+'''
+import pyautogui
+from time import sleep  # permite dormir el programa (en segundos)
+
+''' Para evaluar constantemente usar un while true'''
